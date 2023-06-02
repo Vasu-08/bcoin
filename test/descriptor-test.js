ba8d4334c2986bf4e6c6208e4232526fa6a86be2
Vasu08 <20bcs241@iiitdmj.ac.in>
2023-06-02 10:23:04 +0530

    descriptor: add addr, raw and multisig descriptor

--- test/descriptor-test.js
+++ test/descriptor-test.js
@@ -5,7 +5,16 @@
 const assert = require('bsert');
 const DescriptorParser = require('../lib/wallet/descriptor/parser');
 
-const checkSumTestCases = [
+const testcases = [
+  {
+    desc: `multi(1,03f28773c2d975288bc7d1d205c3748651b075fbc6610e58cddeeddf8f19405aa8,03499fdf9e895e719cfd64e67f07d38e3226aa7b63678949e6e49b241a60e823e4,02d7924d4f7d43ea965a465ae3095ff41131e5946f3c85f79e44adbcf8e27e080e)`,
+    network: 'main',
+    descriptorString: `multi(1,03f28773c2d975288bc7d1d205c3748651b075fbc6610e58cddeeddf8f19405aa8,03499fdf9e895e719cfd64e67f07d38e3226aa7b63678949e6e49b241a60e823e4,02d7924d4f7d43ea965a465ae3095ff41131e5946f3c85f79e44adbcf8e27e080e)#3znta99r`,
+    requireChecksum: false,
+    isRange: false,
+    isSolvable: true,
+    hasPrivateKeys: false
+  },
   {
     desc: `wpkh([d34db33f/84h/0h/0h]0279be667ef9dcbbac55a06295Ce870b07029Bfcdb2dce28d959f2815b16f81798)#qwlqgth7`,
     network: 'main',
@@ -15,6 +24,21 @@ const checkSumTestCases = [
     isRange: false,
     isSolvable: true,
     hasPrivateKeys: false
+  },
+  {
+    desc:`wsh(multi(1,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/1/0/*,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/0/0/*))`,
+    network: 'testnet',
+    requireChecksum: false,
+    descriptorString: `wsh(multi(1,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/1/0/*,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/0/0/*))#8srnyrlv`,
+    isRange: true,
+    isSolvable: true,
+    hasPrivateKeys: false
+  },
+  {
+    desc: `pkh(xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB/2147483648)`,
+    network: 'main',
+    requireChecksum: false,
+    expectedError: `Key path value 2147483648 is out of range`
   },
   {
     desc: `sh(multi(2,[00000000/111'/222]xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL,xpub68NZiKmJWnxxS6aaHmn81bvJeTESw724CRDs6HbuccFQN9Ku14VQrADWgqbhhTHBaohPX4CjNLf9fq9MYo6oDaPPLPxSb7gwQN3ih19Zm4Y/0))#tjq09x4t`,
@@ -51,25 +75,23 @@ const checkSumTestCases = [
 ];
 
 describe('Descriptor Parsing', () => {
-  for (const descriptor of checkSumTestCases) {
-    const {desc, expectedError, requireChecksum, network} = descriptor;
+  for (const testcase of testcases) {
+    const {desc, expectedError, requireChecksum, network} = testcase;
     const require = requireChecksum || false;
     it('Parse descriptor string', () => {
       try {
-        const {isRange, isSolvable, hasPrivateKeys, descriptorString} =
-          descriptor;
-        const parsedDescriptor = DescriptorParser.fromString(
-          desc,
-          network,
-          require
-        );
-        assert.strictEqual(parsedDescriptor.isRange(), isRange);
-        assert.strictEqual(parsedDescriptor.isSolvable(), isSolvable);
-        assert.strictEqual(parsedDescriptor.hasPrivateKeys(), hasPrivateKeys);
-        assert.strictEqual(parsedDescriptor.getString(), descriptorString);
+        const {isRange, isSolvable, hasPrivateKeys, descriptorString} = testcase;
+        const descriptor = DescriptorParser.fromString(desc, network, require);
+        // console.log(descriptor);
+        assert.strictEqual(descriptor.isRange(), isRange);
+        assert.strictEqual(descriptor.isSolvable(), isSolvable);
+        assert.strictEqual(descriptor.hasPrivateKeys(), hasPrivateKeys);
+        assert.strictEqual(descriptor.toString(), descriptorString);
       } catch (e) {
         assert.strictEqual(e.message, expectedError);
       }
     });
   }
 });
+
+// "wsh(multi(1,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/1/0/*,tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B/0/0/*))", isrange=True, issolvable=True, hasprivatekeys=False)
